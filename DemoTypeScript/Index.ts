473db30cb4cee12e9f19823a358bbd19fded5c99
Abhijith Das <65940178+abhijith-das@users.noreply.github.com>
2023-09-21 17:58:03 +0530

    Update Index.ts for prompt

--- DemoTypeScript/Index.ts
+++ DemoTypeScript/Index.ts
@@ -1,24 +1,49 @@
+//importing class files
 import { Rocket } from "./Rocket.js";
 import { Astronaut } from "./Astronaut.js";
 import { Cargo } from "./Cargo.js";
 
-let rocketObj = new Rocket("Racoon",200);
-let cargo = new Cargo(50,"fuel");
 
+let rocketName:string = prompt("Enter rocket name");
+let maxWeight:string = prompt("Enter max capacity");
 
+var maxCapacity = parseInt(maxWeight)
+
+//create rocket object
+let rocketObj = new Rocket(rocketName,maxCapacity);
+
+//create cargo object
+
+let carWt:number = parseInt(prompt("Enter cargo weight"))
+let carName:string = prompt("Enter material name")
+let cargo = new Cargo(carWt,carName);
+
+//checking and adding cargo
 if(rocketObj.addCargo(cargo)){
     console.log(cargo.material + " added");
+    alert(cargo.material + " added")
 }
 else{
     console.log("Cargo cannot be added");
+    alert("Cargo cannot be added")
 
 }
-let astr = new Astronaut(120,"Das");
+
+//astronaut object creation
+
+let astrName:string = prompt("Enter astronaut name: ")
+let astrWt:number = parseInt(prompt("Enter weight: "))
+let astr = new Astronaut(astrWt,astrName);
+
+
+//checking and adding astronaut
 
 if(rocketObj.addAstronaut(astr)){
     console.log(astr.name + " added");
+    alert(astr.name + " added");
 }
 else{
     console.log("Astronaut cannot be added");
+    alert("Astronaut cannot be added");
 }
 
